9124ee18fcc74c0cf3b8c2a30c91a83345d831a8
Willian Schiavoni <willian.a.schiavoni@gmail.com>
2018-09-18 09:28:48 -0300

    aleteração produto/add

--- application/views/produto/add.php
+++ application/views/produto/add.php
@@ -8,169 +8,186 @@
     </li>
     <li class="breadcrumb-item active">Adicionar</li>
 </ol>
-
 <div class="card">
     <div class="card-header">
         <div class="card-title">
             <div class="row">
-                <div class="col-md-8">
-                    <h3>Produto</h3>
-                </div>
-
-                <div class="col-md-4">
-
-                </div>
+                <div class="col-md-12">
+                   <ul class="nav nav-tabs">
+					<li class="nav-item">
+						<a class="nav-link active" href="">Dados Gerais</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="<?php echo base_url('estoque')?>">Estoque</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="<?php echo base_url('Fornecedor')?>">Fornecedor</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Arquivo</a>
+					</li>
+					</ul>
+                </div>               
             </div> 
-
         </div>
-    </div>       
-    <div class="container-fluid">
-        <div class="card-body"> 
+    </div>  
+    <div class="">	
+        <div class="container-fluid">
+            <div class="card-body"> 
+			  <?php echo form_open('produto/add',array("class"=>"form-horizontal")); ?>
+		        <div class="">
+					<div class="row">                      
+							<div class="container-fluid">
+								<div class="form-group col-md-3">
+									<label for="foto" class="col-md-12 control-label">Foto</label>
+									<div class="">
+										<input type="text" name="foto" value="<?php echo $this->input->post('foto'); ?>" class="form-control" id="foto" />
+									</div>
+								</div>
+							</div>						
+					    <div class="form-group col-md-3">
+							<label for="nome" class="col-md-12 control-label">Código (SKU)</label>
+							<div class="">
+								<input type="text" name="nome" value="<?php echo $this->input->post('nome'); ?>" class="form-control" id="nome" />
+							</div>
+						</div>
+						<div class="form-group col-md-3">
+							<label for="IDGrupo" class="col-md-12 control-label">Grupo</label>
+							<div class="">
+								<select name="IDGrupo" class="form-control">
+									<option value="">Selecione Grupo</option>
+									<?php 
+									foreach($all_grupos as $grupo)
+									{
+										$selected = ($grupo['IDGrupo'] == $this->input->post('IDGrupo')) ? ' selected="selected"' : "";
 
-            <?php echo form_open('produto/add',array("class"=>"form-horizontal")); ?>
+										echo '<option value="'.$grupo['IDGrupo'].'" '.$selected.'>'.$grupo['descricao'].'</option>';
+									} 
+									?>
+								</select>
+							</div>
+						</div>
+						<div class="form-group col-md-3">
+							<label for="IDCategoria" class="col-md-12 control-label">Categoria</label>
+							<div class="">
+								<select name="IDCategoria" class="form-control">
+									<option value="">Selecione Categoria</option>
+									<?php 
+									foreach($all_categorias as $categoria)
+									{
+										$selected = ($categoria['IDCategoria'] == $this->input->post('IDCategoria')) ? ' selected="selected"' : "";
 
-            <div class="form-group">
-                <label for="IDGrupo" class="col-md-4 control-label">Grupo</label>
-                <div class="col-md-8">
-                    <select name="IDGrupo" class="form-control">
-                        <option value="">Selecione Grupo</option>
-                        <?php 
-                        foreach($all_grupos as $grupo)
-                        {
-                            $selected = ($grupo['IDGrupo'] == $this->input->post('IDGrupo')) ? ' selected="selected"' : "";
+										echo '<option value="'.$categoria['IDCategoria'].'" '.$selected.'>'.$categoria['descricao'].'</option>';
+									} 
+									?>
+								</select>
+							</div>
+						</div>
+						<div class="form-group col-md-3">
+							<label for="IDMarcas" class="col-md-12 control-label">Marca</label>
+							<div class="">
+								<select name="IDMarcas" class="form-control">
+									<option value="">Selecione Marca</option>
+									<?php 
+									foreach($all_marcas as $marca)
+									{
+										$selected = ($marca['IDMarcas'] == $this->input->post('IDMarcas')) ? ' selected="selected"' : "";
 
-                            echo '<option value="'.$grupo['IDGrupo'].'" '.$selected.'>'.$grupo['descricao'].'</option>';
-                        } 
-                        ?>
-                    </select>
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="IDCategoria" class="col-md-4 control-label">Categoria</label>
-                <div class="col-md-8">
-                    <select name="IDCategoria" class="form-control">
-                        <option value="">Selecione Categoria</option>
-                        <?php 
-                        foreach($all_categorias as $categoria)
-                        {
-                            $selected = ($categoria['IDCategoria'] == $this->input->post('IDCategoria')) ? ' selected="selected"' : "";
+										echo '<option value="'.$marca['IDMarcas'].'" '.$selected.'>'.$marca['Descricao'].'</option>';
+									} 
+									?>
+								</select>
+							</div>
+						</div>
+						<div class="form-group col-md-6">
+							<label for="IDfornecedor" class="col-md-12 control-label">Fornecedor</label>
+							<div class="">
+								<select name="IDfornecedor" class="form-control">
+									<option value="">Selecione Fornecedor</option>
+									<?php 
+									foreach($all_fornecedores as $fornecedor)
+									{
+										$selected = ($fornecedor['IDFornecedor'] == $this->input->post('IDfornecedor')) ? ' selected="selected"' : "";
 
-                            echo '<option value="'.$categoria['IDCategoria'].'" '.$selected.'>'.$categoria['descricao'].'</option>';
-                        } 
-                        ?>
-                    </select>
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="IDMarcas" class="col-md-4 control-label">Marca</label>
-                <div class="col-md-8">
-                    <select name="IDMarcas" class="form-control">
-                        <option value="">Selecione Marca</option>
-                        <?php 
-                        foreach($all_marcas as $marca)
-                        {
-                            $selected = ($marca['IDMarcas'] == $this->input->post('IDMarcas')) ? ' selected="selected"' : "";
+										echo '<option value="'.$fornecedor['IDFornecedor'].'" '.$selected.'>'.$fornecedor['nome'].'</option>';
+									} 
+									?>
+								</select>
+							</div>
+						</div>
+						<div class="form-group col-md-3">
+							<label for="IDUnidade" class="col-md-12 control-label">Unidade</label>
+							<div class="">
+								<select name="IDUnidade" class="form-control">
+									<option value="">Selecione Unidade</option>
+									<?php 
+									foreach($all_unidades as $unidade)
+									{
+										$selected = ($unidade['IDUnidades'] == $this->input->post('IDUnidade')) ? ' selected="selected"' : "";
 
-                            echo '<option value="'.$marca['IDMarcas'].'" '.$selected.'>'.$marca['Descricao'].'</option>';
-                        } 
-                        ?>
-                    </select>
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="IDfornecedor" class="col-md-4 control-label">Fornecedor</label>
-                <div class="col-md-8">
-                    <select name="IDfornecedor" class="form-control">
-                        <option value="">Selecione Fornecedor</option>
-                        <?php 
-                        foreach($all_fornecedores as $fornecedor)
-                        {
-                            $selected = ($fornecedor['IDFornecedor'] == $this->input->post('IDfornecedor')) ? ' selected="selected"' : "";
-
-                            echo '<option value="'.$fornecedor['IDFornecedor'].'" '.$selected.'>'.$fornecedor['nome'].'</option>';
-                        } 
-                        ?>
-                    </select>
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="IDUnidade" class="col-md-4 control-label">Unidade</label>
-                <div class="col-md-8">
-                    <select name="IDUnidade" class="form-control">
-                        <option value="">Selecione Unidade</option>
-                        <?php 
-                        foreach($all_unidades as $unidade)
-                        {
-                            $selected = ($unidade['IDUnidades'] == $this->input->post('IDUnidade')) ? ' selected="selected"' : "";
-
-                            echo '<option value="'.$unidade['IDUnidades'].'" '.$selected.'>'.$unidade['descricao'].'</option>';
-                        } 
-                        ?>
-                    </select>
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="nome" class="col-md-4 control-label">Nome</label>
-                <div class="col-md-8">
-                    <input type="text" name="nome" value="<?php echo $this->input->post('nome'); ?>" class="form-control" id="nome" />
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="codigo" class="col-md-4 control-label">Código</label>
-                <div class="col-md-8">
-                    <input type="text" name="codigo" value="<?php echo $this->input->post('codigo'); ?>" class="form-control" id="codigo" />
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="precoCusto" class="col-md-4 control-label">Preço Custo</label>
-                <div class="col-md-8">
-                    <input type="text" name="precoCusto" value="<?php echo $this->input->post('precoCusto'); ?>" class="form-control" id="precoCusto" />
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="precoVenda" class="col-md-4 control-label">Preço Venda</label>
-                <div class="col-md-8">
-                    <input type="text" name="precoVenda" value="<?php echo $this->input->post('precoVenda'); ?>" class="form-control" id="precoVenda" />
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="situacao" class="col-md-4 control-label">Situação</label>
-                <div class="col-md-8">
-                    <input type="text" name="situacao" value="<?php echo $this->input->post('situacao'); ?>" class="form-control" id="situacao" />
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="descricao" class="col-md-4 control-label">Descrição</label>
-                <div class="col-md-8">
-                    <input type="text" name="descricao" value="<?php echo $this->input->post('descricao'); ?>" class="form-control" id="descricao" />
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="codBarras" class="col-md-4 control-label">Cod. Barras</label>
-                <div class="col-md-8">
-                    <input type="text" name="codBarras" value="<?php echo $this->input->post('codBarras'); ?>" class="form-control" id="codBarras" />
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="foto" class="col-md-4 control-label">Foto</label>
-                <div class="col-md-8">
-                    <input type="text" name="foto" value="<?php echo $this->input->post('foto'); ?>" class="form-control" id="foto" />
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="dataCadastro" class="col-md-4 control-label">Data Cadastro</label>
-                <div class="col-md-8">
-                    <input type="text" name="dataCadastro" value="<?php echo $this->input->post('dataCadastro'); ?>" class="form-control" id="dataCadastro" />
-                </div>
-            </div>
-
-            <div class="form-group">
-                <div class="col-sm-offset-4 col-sm-8">
-                    <button type="submit" class="btn btn-success">Salvar</button>
-                </div>
-            </div>
-
-            <?php echo form_close(); ?>
-                 </div>
-    </div>
+										echo '<option value="'.$unidade['IDUnidades'].'" '.$selected.'>'.$unidade['descricao'].'</option>';
+									} 
+									?>
+								</select>
+							</div>
+						</div>						
+						<div class="form-group col-md-3">
+							<label for="codigo" class="col-md-12 control-label">Formato</label>
+							<div class="">
+								<input type="text" name="codigo" value="<?php echo $this->input->post(''); ?>" class="form-control" id="codigo" />
+							</div>
+						</div>
+						<div class="form-group col-md-3">
+							<label for="codigo" class="col-md-12 control-label">Unidade</label>
+							<div class="">
+								<input type="text" name="codigo" value="<?php echo $this->input->post(''); ?>" class="form-control" id="codigo" />
+							</div>
+						</div>
+						<div class="form-group col-md-3">
+							<label for="precoCusto" class="col-md-12 control-label">Preço Custo</label>
+							<div class="">
+								<input type="text" name="precoCusto" value="<?php echo $this->input->post('precoCusto'); ?>" class="form-control" id="precoCusto" />
+							</div>
+						</div>
+						<div class="form-group col-md-3">
+							<label for="precoVenda" class="col-md-12 control-label">Preço Venda</label>
+							<div class="">
+								<input type="text" name="precoVenda" value="<?php echo $this->input->post('precoVenda'); ?>" class="form-control" id="precoVenda" />
+							</div>
+						</div>
+						<div class="form-group col-md-3">
+							<label for="situacao" class="col-md-12 control-label">Situação</label>
+							<div class="">
+								<input type="text" name="situacao" value="<?php echo $this->input->post('situacao'); ?>" class="form-control" id="situacao" />
+							</div>
+						</div>
+						<div class="form-group col-md-6">
+							<label for="descricao" class="col-md-12 control-label">Descrição</label>
+							<div class="">
+								<input type="text" name="descricao" value="<?php echo $this->input->post('descricao'); ?>" class="form-control" id="descricao" />
+							</div>
+						</div>
+						<div class="form-group col-md-6">
+							<label for="codBarras" class="col-md-12 control-label">Cod. Barras</label>
+							<div class="">
+								<input type="text" name="codBarras" value="<?php echo $this->input->post('codBarras'); ?>" class="form-control" id="codBarras" />
+							</div>
+						</div>
+						<div class="form-group col-md-3">
+							<label for="dataCadastro" class="col-md-12 control-label">Data Cadastro</label>
+							<div class="">
+								<input type="date" name="dataCadastro" value="<?php echo $this->input->post('dataCadastro'); ?>" class="form-control" id="dataCadastro" />
+							</div>
+						</div>												
+					</div>	
+		        </div>
+				<div class="form-group col-md-5">
+					<div class="col-sm-offset-4 col-sm-8">
+						<button type="submit" class="btn btn-success">Salvar</button>
+					</div>
+				</div>
+              <?php echo form_close(); ?>
+            </div>			 
+        </div>
+    </div>	
 </div>

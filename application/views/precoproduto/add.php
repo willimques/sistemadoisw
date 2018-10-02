7574a893a81ce84699388087c474f6c621769c9e
Willian Marques <lwo.marques@gmail.com>
2018-10-01 23:54:14 -0300

    fechamento consignado

--- application/views/precoproduto/add.php
+++ application/views/precoproduto/add.php
@@ -1,7 +1,40 @@
 
 <?php echo form_open('precoproduto/add',array("class"=>"form-horizontal")); ?>
+<div class="form-group col-md-6">
+    <label> Selecione o Produto</label>
+    <div class="">
+        <select name="IDPreco" id="IDPreco" class="form-control">
+            <option value="">Selecione</option>
+            <?php 
+            foreach($all_tabelas as $tab)
+            {
+                $selected = ($tab['IDPreco'] == $this->input->post('IDPreco')) ? ' selected="selected"' : "";
 
-<div class="form-group">
+                echo '<option value="'.$tab['IDPreco'].'" '.$selected.'>'.$tab['descricao'].'</option>';
+            } 
+            ?>
+        </select>
+    </div>
+    <span class="text-danger"><?php echo form_error('IDProduto');?></span>
+</div>
+<div class="form-group col-md-6">
+    <label> Selecione o Produto</label>
+    <div class="">
+        <select name="IDProduto" id="IDProduto" class="form-control">
+            <option value="">Selecione</option>
+            <?php 
+            foreach($all_produtos as $P)
+            {
+                $selected = ($P['IDProduto'] == $this->input->post('IDProduto')) ? ' selected="selected"' : "";
+
+                echo '<option value="'.$P['IDProduto'].'" '.$selected.'>'.$P['descricao'].'</option>';
+            } 
+            ?>
+        </select>
+    </div>
+    <span class="text-danger"><?php echo form_error('IDProduto');?></span>
+</div>
+<div class="form-group col-md-9">
     <label for="valor" class="col-md-4 control-label">Valor</label>
     <div class="col-md-8">
         <input type="text" name="valor" value="<?php echo $this->input->post('valor'); ?>" class="form-control" id="valor" />
@@ -10,7 +43,7 @@
 
 <div class="form-group">
     <div class="col-sm-offset-4 col-sm-8">
-        <button type="submit" class="btn btn-success">Save</button>
+        <button type="submit" class="btn btn-success">Salvar</button>
     </div>
 </div>
 

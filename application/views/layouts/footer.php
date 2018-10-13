2f0af50d7007333026e1a2d290baef228a2e5fea
Willian Marques <lwo.marques@gmail.com>
2018-10-13 17:23:20 -0300

    projetoweb

--- application/views/layouts/footer.php
+++ application/views/layouts/footer.php
@@ -69,9 +69,4 @@
 <script src="<?php echo base_url('assets/template/js/cliente.js')?>"></script>
 
 
-
-
-
-
-
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
